beca659d124611b4c26f41307ddfac81523330ca
Paul <popaulol2006@gmail.com>
2021-02-12 16:47:04 +0100

    Done

---
+++ Bot.py
@@ -0,0 +1,49 @@
+"""Simple Discord Bot for Using TeX/LaTeX in Discord, needs a modern Tex Distribution to run.
+I recommend using this: https://miktex.org/download and restarting the System afterwards to correctly add it to PATH"""
+import discord
+import sympy
+import time
+import os
+TOKEN = "NzU0MDY0ODM3OTk0MDg2NTIy.X1vTmw.ubUArGVcPOxOZedlrSb65NytOZ4"
+Command = ";LaTeX"
+
+print("Started")
+
+err = "No Error occurred so far!"
+
+
+class Bot(discord.Client):
+    async def on_ready(self):
+        print("online: " + str(self))
+
+    async def on_message(self, message):
+        curr_time = time.perf_counter()
+        global err
+        if message.content.lower().startswith(Command.lower()):
+            try:
+                sympy.preview(message.content[len(Command) + 1:], viewer="file", filename=f"rendered-{curr_time}.png")
+                await message.channel.send(file=discord.File(f"rendered-{curr_time}.png"))
+                time.sleep(2)
+                os.remove(f"rendered-{curr_time}.png")
+            except RuntimeError as e:
+                print(e)
+                err = e
+                await message.channel.send("An Error during the LaTeX Rendering accoured" +
+                                           ", if you want to see the Error Type 'y' otherwise irgnore this message.")
+
+        elif message.content.lower() == "y":
+            char_count = 0
+            curr_msg = ""
+            for char in str(err):
+
+                curr_msg = curr_msg + char
+                char_count += 1
+                if char_count >= 1999:
+                    await message.channel.send(curr_msg)
+                    char_count = 0
+                    curr_msg = ""
+            await message.channel.send(curr_msg)
+
+
+Client = Bot()
+Client.run(TOKEN)
